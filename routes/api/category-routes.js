fdc3b9defa642f5587c68ca8eb96e43c7e2b4f87
jchoi10 <noreply>
2022-11-08 00:41:04 -0500

    category router almost done

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
       attributes: ['product_name']
     }
   })
-  .then(CategoryData => res.json(CategoryData))
+  .then(categoryData => res.json(categoryData))
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
@@ -31,7 +31,7 @@ router.get('/:id', (req, res) => {
       attributes: ['category_id']
     }
   })
-  .then(CategoryData => res.json(CategoryData))
+  .then(categoryData => res.json(categoryData))
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
@@ -40,10 +40,37 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(categoryData => res.json(categoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!'});
+      return;
+    }
+    res.json(categoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
